be691c406d13a6f6f42ab0a52e841ff5cd9e9574
Tom Bereknyei <tomberek@gmail.com>
2024-08-04 02:15:01 -0400

    asfas

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   outputs = inputs: {
     hydraJobs = ({inp ? inputs.nixpkgs}: {
-      hello =  {
-        x86-64-linux = (if builtins.isString inp then builtins.getFlake inp else inp).legacyPackages.x86_64-linux.hello;
+      ran =  {
+        x86-64-linux = (if builtins.isString inp then builtins.getFlake inp else inp).legacyPackages.x86_64-linux.ran;
       };
     });
   };
