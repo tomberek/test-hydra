06fea02733eaee510268222b62e56222aca5b6e6
Tom Bereknyei <tomberek@gmail.com>
2024-08-04 01:48:28 -0400

    asfas

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "nixpkgs";
   outputs = inputs: {
     hydraJobs = ({
-      hello.x86-64-linux = nixpkgs.legacyPackages.x86_64-linux.hello;
+      hello.x86-64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
     });
   };
 }
