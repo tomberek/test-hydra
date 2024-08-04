8e0e0f7307511811a4188a8545f067e658bf1d09
Tom Bereknyei <tomberek@gmail.com>
2024-08-04 02:00:36 -0400

    asfas

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   outputs = inputs: {
     hydraJobs = ( {
       hello.x86-64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
